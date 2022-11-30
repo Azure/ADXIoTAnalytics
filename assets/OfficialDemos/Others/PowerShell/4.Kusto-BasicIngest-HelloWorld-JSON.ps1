c0529979e9e0bfe6a3fb6632f9ace2e66b69f2bb
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-11-30 17:56:20 -0500

    Update 4.Kusto-BasicIngest-HelloWorld-JSON.ps1

--- assets/OfficialDemos/Others/PowerShell/4.Kusto-BasicIngest-HelloWorld-JSON.ps1
+++ assets/OfficialDemos/Others/PowerShell/4.Kusto-BasicIngest-HelloWorld-JSON.ps1
@@ -4,27 +4,30 @@ $null = [System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Data.dll")
 $null = [System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Ingest.dll")
 $null = [System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Cloud.Platform.dll")
 class T {
-    [string] $json
+    [string] $column1
     T(
-        [string] $json
+        [string] $column1
     ){
-        $this.json=$json
+        $this.column1=$column1
     }
 }
 
 #  destination
 $uri = "https://ingest-kvc43f0ee6600e24ef2b0e.southcentralus.kusto.windows.net;Fed=True"
 $db = "MyDatabase"
-$t = "Counter_raw"
+$t = "MyTable"
+
+# kql https://aka.ms/adx.free
+# .create table MyTable (column1:string)
 
 #  get data
-[T[]]$x = '[{"msg":"hello world!"}]'
+[T[]]$x = 'hello world!'
 
 #  ingest
 $s = [Kusto.Data.KustoConnectionStringBuilder]::new($uri, $db)
 $c = [Kusto.Ingest.KustoIngestFactory]::CreateQueuedIngestClient($s)
 $p = [Kusto.Ingest.KustoQueuedIngestionProperties]::new($db, $t)
-$dr = New-Object Kusto.Cloud.Platform.Data.EnumerableDataReader[T] ($x, 'json')
+$dr = New-Object Kusto.Cloud.Platform.Data.EnumerableDataReader[T] ($x, 'column1')
 $r = $c.IngestFromDataReaderAsync($dr,$p)
 $r
 $r.Result.GetIngestionStatusCollection()
