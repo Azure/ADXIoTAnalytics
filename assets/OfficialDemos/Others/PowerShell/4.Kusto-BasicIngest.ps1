0f3a7544ec1d04c299cdef84b94ce10854ceab05
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-11-30 00:19:43 -0500

    Update 4.Kusto-BasicIngest.ps1

--- assets/OfficialDemos/Others/PowerShell/4.Kusto-BasicIngest.ps1
+++ assets/OfficialDemos/Others/PowerShell/4.Kusto-BasicIngest.ps1
@@ -1,26 +1,25 @@
 #  dependencies
-$packagesRoot = "C:\Microsoft.Azure.Kusto.Tools\tools\net5.0"
-[System.Reflection.Assembly]::LoadFrom("$packagesRoot\Kusto.Data.dll")
+$pkgroot = "C:\Microsoft.Azure.Kusto.Tools\tools\net5.0"
+[System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Data.dll")
 
-#  destination
-$IngestUrl = "https://kvc43f0ee6600e24ef2b0e.southcentralus.kusto.windows.net;Fed=True"
-$DatabaseName = "MyDatabase"
-$TableName = "Counter"
-$FilePath
+#  collect data
+$x = (Get-Counter).CounterSamples | Select-Object Timestamp, Path, InstanceName, CookedValue | ConvertTo-Json 
+Write-Host $x
 
-#  function
-$KustoConnectionStringBuilder = [Kusto.Data.KustoConnectionStringBuilder]::new($IngestUrl,$DatabaseName)
-$KustoClient = [Kusto.Ingest.KustoIngestFactory]::CreateQueuedIngestClient($KustoConnectionStringBuilder)
-$IngestionProperties = [Kusto.Ingest.KustoQueuedIngestionProperties]::new(
-    $DatabaseName, 
-    $TableName
-)
-$IngestionProperties.Format = [Kusto.Data.Common.DataSourceFormat]::csv 
-$IngestionProperties.IgnoreFirstRecord = $true
-$IngestionResult = $KustoClient.IngestFromStorageAsync(
-    $FilePath,
-    $IngestionProperties
-)
-$IngestionResult.Result.GetIngestionStatusCollection()
+#  set destination
+$uri = "https://kvc43f0ee6600e24ef2b0e.southcentralus.kusto.windows.net;Fed=True"
+$db = "MyDatabase"
+$t = "Counter"
 
+#  ingest
+$s = [Kusto.Data.KustoConnectionStringBuilder]::new($uri, $db)
+$c = [Kusto.Ingest.KustoIngestFactory]::CreateQueuedIngestClient($s)
+$p = [Kusto.Ingest.KustoQueuedIngestionProperties]::new($db, $t)
+$p.Format = [Kusto.Data.Common.DataSourceFormat]::json 
+# $p.IgnoreFirstRecord = $true
+$r = $c.IngestFromStorageAsync($x,$p)
+$r.Status
+$r = $c.IngestFromDataReaderAsync($x,$p)
+$r.Status
 
+# MethodException: Cannot find an overload for "IngestFromDataReaderAsync" and the argument count: "2".
