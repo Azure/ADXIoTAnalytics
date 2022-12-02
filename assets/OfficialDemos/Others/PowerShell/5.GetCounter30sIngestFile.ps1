424b67ef58c591bd498ca30d818c696e29f0974b
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-12-02 11:39:29 -0500

    Update 5.GetCounter30sIngestFile.ps1

--- assets/OfficialDemos/Others/PowerShell/5.GetCounter30sIngestFile.ps1
+++ assets/OfficialDemos/Others/PowerShell/5.GetCounter30sIngestFile.ps1
@@ -6,22 +6,27 @@ $null = [System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Ingest.dll")
 #  destination
 $uri = "https://ingest-kvc43f0ee6600e24ef2b0e.southcentralus.kusto.windows.net;Fed=True"
 $db = "MyDatabase"
-$t = "Counter_raw"
+$t = "Counter"
 
 #  get data
+$fp = 'C:\hiram_msft\ADXIoTAnalytics\assets\OfficialDemos\Others\PowerShell\Counter.csv'
+Remove-Item $fp -Force
 $timeout = new-timespan -Seconds 30
 $sw = [diagnostics.stopwatch]::StartNew()
 while ($sw.elapsed -lt $timeout){
-  (Get-Counter).CounterSamples | Select-Object Timestamp, Path, InstanceName, CookedValue | ConvertTo-Json | Out-File Counter.json -Force -Append
+  (Get-Counter).CounterSamples `
+  | Select-Object Timestamp, Path, InstanceName, CookedValue `
+  | ConvertTo-Csv  -NoTypeInformation `
+  | Select-Object -Skip 1 `
+  | Out-File $fp -Force -Append 
   start-sleep -Seconds 1
 }
-$fp = 'C:\hiram_msft\ADXIoTAnalytics\assets\OfficialDemos\Others\PowerShell\Counter.json'
 
 #  ingest
 $s = [Kusto.Data.KustoConnectionStringBuilder]::new($uri, $db)
 $c = [Kusto.Ingest.KustoIngestFactory]::CreateQueuedIngestClient($s)
 $p = [Kusto.Ingest.KustoQueuedIngestionProperties]::new($db, $t)
-$p.Format = [Kusto.Data.Common.DataSourceFormat]::json
+$p.Format = [Kusto.Data.Common.DataSourceFormat]::csv
 $p.IgnoreFirstRecord = $false
 $r = $c.IngestFromStorageAsync($fp,$p)
 $r
