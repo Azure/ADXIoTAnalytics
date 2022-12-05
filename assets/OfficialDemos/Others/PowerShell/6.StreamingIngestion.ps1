27295f26f3b8b0d0680f620460c6e2485abd6e65
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-12-05 10:09:48 -0500

    Update 6.StreamingIngestion.ps1

--- assets/OfficialDemos/Others/PowerShell/6.StreamingIngestion.ps1
+++ assets/OfficialDemos/Others/PowerShell/6.StreamingIngestion.ps1
@@ -5,17 +5,23 @@ $null = [System.Reflection.Assembly]::LoadFrom("$pkgroot\Kusto.Ingest.dll")
 
 $uri = "https://adxpm10774.eastus.kusto.windows.net;Fed=True"
 $db = "sentinel"
-$t = "Counter_raw"
+$t = "Counter_raw2"
 
 #  client
 $s = [Kusto.Data.KustoConnectionStringBuilder]::new($uri, $db)
 $c = [Kusto.Ingest.KustoIngestFactory]::CreateStreamingIngestClient($s)
 $p = [Kusto.Ingest.KustoIngestionProperties]::new($db, $t)
+$p.Format = [Kusto.Data.Common.DataSourceFormat]::multijson
+# $p.IgnoreFirstRecord = $true
 $ms = [System.IO.MemoryStream]::new()
 $sw = [System.IO.StreamWriter]::new($ms)
-$sw.Write({(Get-Counter).CounterSamples | Select-Object Timestamp, Path, InstanceName, CookedValue | ConvertTo-Json})
+$t = (Get-Counter).CounterSamples | Select-Object Timestamp, Path, InstanceName, CookedValue | % { @{Data = $_} } | ConvertTo-Json
+# echo $text
+$ms.Position = 0
+$sw.Write($t)
+$sw.Flush()
+$ms.Position = 0
+# echo $ms
 $r = $c.IngestFromStreamAsync($ms,$p)
 $r
 $r.Result.GetIngestionStatusCollection()
-
-# Bug: Ingestion Status Skipped
